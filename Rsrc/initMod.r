fd25508d019f20d63176b3d2cc7d720225ab240e
Lindfors <lolindfo@ad.helsinki.fi>
2021-02-10 13:53:26 +0200

    Comments on the initMod.R file that will hopefully makes understanding the script easier.

--- Rsrc/initMod.r
+++ Rsrc/initMod.r
@@ -1,18 +1,24 @@
+# ABOUT THIS SCRIPT
+# This script composes an input matrixes (spicies spesific) that can be fed into the PREBAS model. The matrixes are: initPrebasSp, initPrebasP, initPrebasAll
+# Each of these matrixes includes a table of initial parameters (see initVarP below) describing the site/stand and also weather time series data sets: Preciptran,VPDtran,TAirtran,PARtran
+# Initial site parameters come from initVarP.csv,initVarSp.csv,initVarAll.csv files and time seris data from weatherTran.rdata file
+# This script is run by runModel.R and runModel&GV plots.R
+
 source("Rsrc/settings.r")
 
 nSites <- 7
 siteInfo <- matrix(c(NA,NA,3,160,0,0,20,3,3,413,0.45,0.118),nSites,12,byrow = T)
 
-initVarAll <- read.csv("inputs/initVarAll.csv",header = T, row.names = 1)
+initVarAll <- read.csv("inputs/initVarAll.csv",header = T, row.names = 1)                                       # Loading site spesific initial values, mixed forest?
 # initVarAll[5,] <- initVarAll[5,] * 2
 # if(oldVer)
 # siteInfo <- matrix(c(NA,NA,3,160,0,0,20,3,3),nSites,9,byrow = T)
 # siteInfoP <- siteInfoSp <- siteInfo; siteInfoP[,8:9] <- siteInfoSp[,8:9] <- 1
-initVarAll <- aperm(array(as.matrix(initVarAll),dim=c(7,3,7)),c(3,1,2))
+initVarAll <- aperm(array(as.matrix(initVarAll),dim=c(7,3,7)),c(3,1,2))                                         # Changing the matrix structure
                     
-initVarP <- read.csv("inputs/initVarP.csv",header = T, row.names = 1)
+initVarP <- read.csv("inputs/initVarP.csv",header = T, row.names = 1)                                           # Pine. Loading site spesific initial values, mixed forest?
 initVarP <- aperm(array(as.matrix(initVarP),dim=c(7,1,7)),c(3,1,2))
-initVarSp <- read.csv("inputs/initVarSp.csv",header = T, row.names = 1)
+initVarSp <- read.csv("inputs/initVarSp.csv",header = T, row.names = 1)                                         # Spruce. Loading site spesific initial values, mixed forest?
 initVarSp <- aperm(array(as.matrix(initVarSp),dim=c(7,1,7)),c(3,1,2))
 
 ###proc data weather###
@@ -24,8 +30,8 @@ initVarSp <- aperm(array(as.matrix(initVarSp),dim=c(7,1,7)),c(3,1,2))
 # Preciptran <- Precip[transect,]
 # CO2tran <- CO2[transect,]
 # save(PARtran,TAirtran,Preciptran,VPDtran,CO2tran,file="inputs/weatherTran.rdata")
-load("inputs/weatherTran.rdata")
-xx <- c((which(PARtran == -999)[1]-7):(which(PARtran == -999)[1]-1),
+load("inputs/weatherTran.rdata")                                                                                # Loading weather data. This includes data tables (matrix array): CO2tran, PARtran, Preciptran, TAirtran, VPDtran. These matrixes appear to hold timeseries data (by columns) for different site types (rows). data.
+xx <- c((which(PARtran == -999)[1]-7):(which(PARtran == -999)[1]-1),                                            # Removing NA values and filling the caps for the loaded weather data
         (which(PARtran == -999)[7]+1):(which(PARtran == -999)[7]+7))
 Preciptran[which(Preciptran == -999)] <- mean(Preciptran[xx])
 VPDtran[which(VPDtran == -999)] <- mean(VPDtran[xx])
@@ -37,8 +43,8 @@ PARtran[which(PARtran == -999)] <- mean(PARtran[xx])
 nSites <- 7; nYears <- 150#*6
 nYearsMS <- rep(nYears,nSites)
 # siteInfo = matrix(c(1,1,3,160,0,0,20,3,3),nSites,9,byrow = T)
-siteInfo[,2] <- siteInfo[,1] <- 1:nSites
-siteInfoP <- siteInfoSp <- siteInfo; siteInfoP[,8:9] <- siteInfoSp[,8:9] <- 1
+siteInfo[,2] <- siteInfo[,1] <- 1:nSites                                                                        # Adding a site no column into matrix of site spesific initial parameters
+siteInfoP <- siteInfoSp <- siteInfo; siteInfoP[,8:9] <- siteInfoSp[,8:9] <- 1                                   # Cloning site parameter matrix for pine and spruce
 # initVarP <- array(c(1,10,1.5,0.5,0.0431969,0.,0.),dim=c(7,nSites,1))
 # initVarSp <- array(c(2,10,1.5,0.5,0.0431969,0.,0.),dim=c(7,nSites,1))
 # initVar <- matrix(c(2,10,1.5,0.5,0.0431969/3,0.,0.),7,3);initVar[1,] <- 1:3
@@ -47,7 +53,7 @@ siteInfoP <- siteInfoSp <- siteInfo; siteInfoP[,8:9] <- siteInfoSp[,8:9] <- 1
 # initVarP <- aperm(initVarP,c(2,1,3))
 # initVarSp <- aperm(initVarSp,c(2,1,3))
 # initVarAll <- aperm(initVarAll,c(3,1,2))
-litterSizeP <- array(0,dim=c(nSites,3,1))
+litterSizeP <- array(0,dim=c(nSites,3,1))                                                                       # Creating litter related 3D arrays for pine, spruce and mix sites
 litterSizeP[,2,] <- 2;litterSizeP[,1,] <- 30
 litterSizeSp <- litterSizeP
 litterSizeAll <- array(0,dim=c(nSites,3,3))
@@ -59,7 +65,7 @@ litterSizeAll[,2,] <- 2
 # load("inputs/parameters.rdata")
 nRep <- ceiling(nYears/(dim(PARtran)[2]/365))
 # ptm <- proc.time()
-initPrebasSp <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoSp,
+initPrebasSp <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoSp,                                     # Spruce. Function appears to collect initial value and times series data into a single package/variable.I guess this will be then fed into the PREBASS model in the 'runModel.R' -script
                              # pCROBAS = pCROBAS,
                              # litterSize = litterSizeSp,pAWEN = parsAWEN,
                                # defaultThin=0,ClCut = 0.,
@@ -71,7 +77,7 @@ initPrebasSp <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoSp,
                                CO2=do.call(cbind, replicate(nRep, CO2tran, simplify=FALSE)),
                                yassoRun = 1)#,lukeRuns = 1)
 
-initPrebasP <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoP,
+initPrebasP <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoP,                                       # Pine. Function appears to collect initial value and times series data into a single package/variable.
                               # pCROBAS = pCROBAS,
                               # litterSize = litterSizeP,pAWEN = parsAWEN,
                               # defaultThin=0,ClCut = 0.,
@@ -83,7 +89,7 @@ initPrebasP <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfoP,
                               CO2=cbind(CO2tran,CO2tran,CO2tran,CO2tran,CO2tran,CO2tran),
                               yassoRun = 1)#,lukeRuns = 1)
 
-initPrebasAll <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfo,
+initPrebasAll <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfo,                                      # Both species? Function appears to collect initial value and times series data into a single package/variable.
                               # pCROBAS = pCROBAS,
                               # litterSize = litterSizeAll,pAWEN = parsAWEN,
                               # defaultThin=0,ClCut = 0.,
@@ -95,4 +101,4 @@ initPrebasAll <- InitMultiSite(nYearsMS = rep(nYears,7),siteInfo=siteInfo,
                               CO2=cbind(CO2tran,CO2tran,CO2tran,CO2tran,CO2tran,CO2tran),
                               yassoRun = 1)#,lukeRuns = 1)
 
-save(initPrebasAll,initPrebasP,initPrebasSp,file="inputs/initPreas.rdata")
+save(initPrebasAll,initPrebasP,initPrebasSp,file="inputs/initPreas.rdata")                                      # Saving these initial value & times series data 'packages' into R workspace files.
